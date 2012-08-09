ca61726c91c470a495932be3689fbc7bcf8faf2f
Charlie Sahlman <csahlman@gmail.com>
2012-08-09 09:13:48 -0400

    Added amazon-aws support

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ gem 'rails', '3.2.3'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'pg', group: :production
-
+gem 'aws-sdk', '~> 1.3.4'
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,13 @@
 class Photo < ActiveRecord::Base
   attr_accessible :avatar, :primary, :caption
-  has_attached_file :avatar, default_url: "/default.jpg",
-  :styles => { :medium => "300x300>", :thumb => "100x100>", :mini => "30x30>" }
+  has_attached_file :avatar,
+  :styles => { :medium => "300x300>", :thumb => "100x100>", :mini => "30x30>", :large => "500x500>" },
+    :storage => :s3,
+    :bucket => 'csahlman',
+    :s3_credentials => {
+      :access_key_id => 'AKIAJWWS4KURC6ODGJGQ',
+      :secret_access_key => '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'
+    }
 
   belongs_to :profile, counter_cache: true
    	
