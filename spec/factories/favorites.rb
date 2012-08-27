130013687d54ec41914fc7c0f6fd95e131ef0d26
Charlie Sahlman <csahlman@gmail.com>
2012-08-27 13:10:43 -0400

    Got rid of factories

--- spec/factories/favorites.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at http://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :favorite do
-    favoriter_id 1
-    favoritee_id 1
-  end
-end
--- spec/factories/winks.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at http://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :wink do
-    sender_id 1
-    receiver_id 1
-  end
-end
